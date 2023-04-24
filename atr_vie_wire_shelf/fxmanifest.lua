8c36ced8be8c9cbfbb6d782b0ce3cbbab271c42b
ATRVIIE <119594378+ATRVIIE@users.noreply.github.com>
2023-04-24 18:01:01 +0200

    Add files via upload

---
+++ atr_vie_wire_shelf/fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+game { 'gta5' }
+
+author 'ATR#4444' 
+
+files {
+  'stream/atr_vie_wire_shelf_set_props.ytyp',
+}
+
+data_file 'DLC_ITYP_REQUEST' 'stream/atr_vie_wire_shelf_set_props.ytyp'
